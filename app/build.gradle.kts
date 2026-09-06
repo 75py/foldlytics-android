0f0743916179dc8c7fcf23314ad035c0a78bb40f
75py <dev.75py@gmail.com>
2026-09-07 07:29:51 +0900

    Bump version code to 11

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "com.nagopy.android.foldlytics"
         minSdk = 29
         targetSdk = 36
-        versionCode = 10
+        versionCode = 11
         versionName = "1.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
